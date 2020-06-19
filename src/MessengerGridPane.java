4ec7c8bd5d0b63aee60c5592ab3c4eb6439040d6
xomnsn <xomnsn@gmail.com>
2020-06-19 21:44:26 +0700

    Created new class MessengerGridPane from repository clone

---
+++ src/MessengerGridPane.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MessengerGridPane extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
